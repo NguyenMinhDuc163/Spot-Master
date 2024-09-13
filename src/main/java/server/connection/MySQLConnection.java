1dc3769cd959032d93dfef0a61b5cd79fc068483
Nguyen Duc <DucNM.B21CN249@stu.ptit.edu.vn>
2024-09-13 10:47:03 +0700

    setup docker compose

--- src/main/java/server/connection/MySQLConnection.java
+++ src/main/java/server/connection/MySQLConnection.java
@@ -14,7 +14,9 @@ public class MySQLConnection implements IDatabaseConnection {
     private final String jdbcHost = dotenv.get("MYSQL_DB_HOST");
     private final String jdbcPort = dotenv.get("MYSQL_DB_PORT");
     private final String jdbcDatabase = dotenv.get("MYSQL_DB_NAME");
-    private final String jdbcURL = "jdbc:mysql://" + jdbcHost + ":" + jdbcPort + "/" + jdbcDatabase + "?useSSL=false";
+    // Thêm `allowPublicKeyRetrieval=true` vào chuỗi kết nối
+    private final String jdbcURL = "jdbc:mysql://" + jdbcHost + ":" + jdbcPort + "/" + jdbcDatabase +
+            "?allowPublicKeyRetrieval=true";
     private final String jdbcUsername = dotenv.get("MYSQL_DB_USERNAME");
     private final String jdbcPassword = dotenv.get("MYSQL_DB_PASSWORD");
     private Connection connection;
